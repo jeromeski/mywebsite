b33673d583bb433393925cd4bfeff7bdafed1785
Jeromeski <jeromegee@gmail.com>
2020-03-15 17:20:17 +0800

    Configured blast-text

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global $*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  componentDidMount() {
+    $('.blast-root').blast({delimiter: 'character', tag: 'span'})
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="blast-root">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
